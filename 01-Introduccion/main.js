c2d7be17aff1891886e270d0e0d5ca3fbf7a7b95
Valeria Dhamary  Manriquez Aravena <valeria.manriquez90@gmail.com>
2021-05-09 22:43:30 -0400

    parte 2

--- 01-Introduccion/main.js
+++ 01-Introduccion/main.js
@@ -3,10 +3,10 @@
 // Preguntar al usuario un nombre
 const nombre = prompt('¿Cuál es tu nombre?');
 //agregar datos en Html
-document.querySelector('#codigo__javascript').innerHTML=`${nombre}, esta aprendiendo JavaScript Moderno`;
+document.querySelector('#codigo__javascript').innerHTML = `${nombre}, esta aprendiendo JavaScript Moderno`;
 //Asigna el valor hacia la variable producto
 const producto = 'Monitor 24 pulgadas';
 //consola
-console.error("Algo salió mal")
-console.warn("Algo puede salir mal")
+console.error("Algo salió mal");
+console.warn("Algo puede salir mal");
 
---
+++ 02-Variables/js/01-app.js
@@ -0,0 +1,8 @@
+//crear variables que pueden transformar con el paso del código
+var product = "Monitor de 24 pulg";
+console.log("primera variable: ", product);
+product = "Monitor de 19 pulgadas";
+console.log("parte alterada: ", product);
+
+//Se puede inicializar sin valor y sumarlo despues
+var disponible;
