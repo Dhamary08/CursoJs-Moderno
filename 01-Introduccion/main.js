a93467c5411a61b600b3ed10814092f9f675fa95
Valeria Dhamary  Manriquez Aravena <valeria.manriquez90@gmail.com>
2021-05-09 22:31:18 -0400

    leccion 1

---
+++ 01-Introduccion/main.js
@@ -0,0 +1,12 @@
+//alert('hola mundo')
+
+// Preguntar al usuario un nombre
+const nombre = prompt('¿Cuál es tu nombre?');
+//agregar datos en Html
+document.querySelector('#codigo__javascript').innerHTML=`${nombre}, esta aprendiendo JavaScript Moderno`;
+//Asigna el valor hacia la variable producto
+const producto = 'Monitor 24 pulgadas';
+//consola
+console.error("Algo salió mal")
+console.warn("Algo puede salir mal")
+
